4ac206336f5abea702519a0aa6bef16fa95f8139
DeMelo828 <164568823+DeMelo828@users.noreply.github.com>
2024-08-27 20:32:07 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,32 +1,22 @@
 import requests
+import os
 
-url_base = "youtube.com"  
+def check_subdomains(url_base):
+    file_path = "ex4/subdomains-top1million-5000.txt"
+    if not os.path.exists(file_path):
+        print(f"O arquivo '{file_path}' não foi encontrado.")
+        return
+    with open(file_path, "r") as arquivo:
+        subdominios = [linha.strip() for linha in arquivo.readlines()[:20]]
+    for subdominio in subdominios:
+        full_url = f"https://{subdominio}.{url_base}"
+        try:
+            response = requests.get(full_url)
+            print(f"URL: {full_url} - Status Code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao acessar {full_url}: {e}")
+check_subdomains("youtube.com")
 
-def fetch_data(request_url):
-    try:
-        response = requests.get(request_url)
-        if response.status_code == 200:
-            print(f"Status code 200 for {request_url}")
-            return response.text  # Retornar o conteúdo da resposta
-        else:
-            print(f'Erro: {response.status_code} para {request_url}')
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f'Ocorreu um erro: {e} para {request_url}')
-        return None
-
-# Abrir e ler o arquivo 'test2.txt'
-with open("test2.txt", "r") as arquivo:
-    for i, linha in enumerate(arquivo):
-        if i < 20:
-            linha = linha.strip()
-            print(f"Lendo URL base: {linha}")
-            full_url = f"https://{linha}.{url_base}"
-            data = fetch_data(full_url)
-            if data:
-                print(data)
-        else:
-            break
 
 
 
